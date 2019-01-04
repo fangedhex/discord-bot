f3dc2a1ca26651b8d847f2398e42a65e8edf6523
Fanged Hex <viste.sylvain@gmail.com>
2019-01-04 13:02:37 +0100

    feature: move to DiscordJS Commando for commands handling

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,25 @@
 */
 
 // Loading .env
-require('dotenv').config()
-import getenv = require("getenv")
+require('dotenv').config();
+import getenv = require("getenv");
 
-const DISCORD_API_KEY = getenv("DISCORD_API_KEY")
-const DISCORD_CHANNEL_ID = "475991437822001162"
+const DISCORD_API_KEY = getenv("DISCORD_API_KEY");
+const DISCORD_CHANNEL_ID = "475991437822001162";
 
-const debug = require("debug")("bot:kernel")
-import * as moment from "moment"
+const debug = require("debug")("bot:kernel");
 
 // Import the discord.js module
-import { Client, RichEmbed, TextChannel } from 'discord.js'
+import { CommandoClient } from 'discord.js-commando'
+import { RichEmbed, TextChannel } from 'discord.js'
 // Create an instance of a Discord client
-const client = new Client();
+const client = new CommandoClient({
+	owner: "176651016660451328"
+});
+
+// Commands loader
+client.commandPrefix = "!";
+client.registry.registerDefaults();
 
 import { Provider } from "./providers/provider"
 
@@ -33,7 +39,7 @@ const providers: Array<Provider> = [
 function doStuff()
 {
 	// Finding the correct channel
-	let channel = client.channels.find(c => c.id === DISCORD_CHANNEL_ID) as TextChannel
+	let channel = client.channels.find(c => c.id === DISCORD_CHANNEL_ID) as TextChannel;
 
 	// Removing all messages
 	channel.fetchMessages(
@@ -41,20 +47,20 @@ function doStuff()
 		limit: 100
 	}).then((messages) =>
 	{
-		channel.bulkDelete(messages)
-	})
-	debug("Messages has been deleted from channel")
+		channel.bulkDelete(messages);
+	});
+	debug("Messages has been deleted from channel");
 
 	providers.forEach((provider) => {
 		provider().then((data) => {
-			let msg = new RichEmbed
-			msg.setTitle(data.title)
-			msg.setDescription(data.content)
-			channel.send(msg)
-			debug(`Sending RichEmbed ${data.title} to Discord`)
+			let msg = new RichEmbed;
+			msg.setTitle(data.title);
+			msg.setDescription(data.content);
+			channel.send(msg);
+			debug(`Sending RichEmbed ${data.title} to Discord`);
 		})
 		.catch((err) => {
-			debug(err)
+			debug(err);
 		})
 	})
 }
@@ -66,23 +72,27 @@ import { CronJob } from "cron"
  * received from Discord
  */
 client.on('ready', () =>
-{	
-	let cronJob = new CronJob("0 0 8 * * *", () => {
-		doStuff()
-		let next = cronJob.nextDates().toISOString().replace(/T/, ' ').replace(/\..+/, '')
-		debug("Waiting until " + next + "(UTC)")
-	}, null, true, "Europe/Paris")
+{
+	client.user.setActivity("!help").then(() => {
+		debug("Activity has been set !");
+	});
 
-	let next = cronJob.nextDates().toISOString().replace(/T/, ' ').replace(/\..+/, '')
-	debug("Waiting until " + next + "(UTC)")
+	let cronJob = new CronJob("0 0 8 * * *", () => {
+		doStuff();
+		let next = cronJob.nextDates().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+		debug("Waiting until " + next + "(UTC)");
+	}, null, true, "Europe/Paris");
+
+	let next = cronJob.nextDates().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+	debug("Waiting until " + next + "(UTC)");
 });
 
 client.on('message', (message) => {
 	if(message.content === "$reload_news") {
-		message.delete()
-		doStuff()
+		message.delete();
+		doStuff();
 	}
-})
+});
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login(DISCORD_API_KEY);
