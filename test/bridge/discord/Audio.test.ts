a9b51206be48cf0d86aa920d1800f9e4bcf8edb0
Sylvain VISTE <viste.sylvain@gmail.com>
2020-08-29 12:19:30 +0200

    test: fix error with ts-ignore

--- test/bridge/discord/Audio.test.ts
+++ test/bridge/discord/Audio.test.ts
@@ -5,36 +5,37 @@ import { Readable } from "stream";
 import { Discord } from "../../../src/bridge/discord/Discord";
 
 describe(Audio, () => {
-    const discord = mock<Discord>();
-    const voiceConnection = mock<VoiceConnection>();
-    const audio = new Audio(discord, voiceConnection);
-    const streamDispatcher = mock<StreamDispatcher>();
+  const discord = mock<Discord>();
+  const voiceConnection = mock<VoiceConnection>();
+  const audio = new Audio(discord, voiceConnection);
+  const streamDispatcher = mock<StreamDispatcher>();
 
-    it("adds to playlist and play it", () => {
-        const stream = mock<Readable>();
-        const onDemandStream = jest.fn(() => stream);
+  it("adds to playlist and play it", () => {
+    const stream = mock<Readable>();
+    const onDemandStream = jest.fn().mockReturnValue(stream);
 
-        voiceConnection.play.mockImplementation(() => streamDispatcher);
+    // @ts-ignore
+    voiceConnection.play.mockReturnValue(streamDispatcher);
 
-        audio.add(onDemandStream);
+    audio.add(onDemandStream);
 
-        expect(onDemandStream).toHaveBeenCalled();
-        expect(voiceConnection.play).toHaveBeenCalledWith(stream, {volume: 1});
-        expect(streamDispatcher.once).toHaveBeenCalledWith("finish", anyFunction());
-    });
+    expect(onDemandStream).toHaveBeenCalled();
+    expect(voiceConnection.play).toHaveBeenCalledWith(stream, { volume: 1 });
+    expect(streamDispatcher.once).toHaveBeenCalledWith("finish", anyFunction());
+  });
 
-    it("pause", () => {
-        audio.pause();
-        expect(streamDispatcher.pause).toHaveBeenCalled();
-    });
+  it("pause", () => {
+    audio.pause();
+    expect(streamDispatcher.pause).toHaveBeenCalled();
+  });
 
-    it("resume", () => {
-        audio.resume();
-        expect(streamDispatcher.resume).toHaveBeenCalled();
-    });
+  it("resume", () => {
+    audio.resume();
+    expect(streamDispatcher.resume).toHaveBeenCalled();
+  });
 
-    it("sets volume", () => {
-        audio.setVolume(0.5);
-        expect(streamDispatcher.setVolume).toHaveBeenCalledWith(0.5);
-    });
+  it("sets volume", () => {
+    audio.setVolume(0.5);
+    expect(streamDispatcher.setVolume).toHaveBeenCalledWith(0.5);
+  });
 });
